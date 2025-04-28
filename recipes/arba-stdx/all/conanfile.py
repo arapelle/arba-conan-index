962285704e31c7fd9d70a45a8df42e2724a11f4d
Aymeric Pellé <aympelle@gmail.com>
2025-04-28 15:36:30 +0200

    arba-stdx: Adds version 0.3.0.

--- recipes/arba-stdx/all/conanfile.py
+++ recipes/arba-stdx/all/conanfile.py
@@ -57,7 +57,10 @@ class ArbaStdxRecipe(ConanFile):
         check_min_cppstd(self, 20)
 
     def requirements(self):
-        self.requires("arba-meta/[^0.3]", transitive_headers=True, transitive_libs=True)
+        if self.version >= Version("0.3.0"):
+            self.requires("arba-meta/[^0.5]", transitive_headers=True, transitive_libs=True)
+        else:
+            self.requires("arba-meta/[^0.3]", transitive_headers=True, transitive_libs=True)
 
     def build_requirements(self):
         self.test_requires("gtest/[^1.14]")
