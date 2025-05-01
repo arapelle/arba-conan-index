1b590f45acd715daaebc5159b67c4c49e9d581fb
Aymeric Pellé <aympelle@gmail.com>
2025-05-01 16:51:05 +0200

    arba-math: Adds version 0.7.0.

--- recipes/arba-math/all/conanfile.py
+++ recipes/arba-math/all/conanfile.py
@@ -53,8 +53,12 @@ class ArbaMathRecipe(ConanFile):
         check_min_cppstd(self, 20)
 
     def requirements(self):
-        self.requires("arba-meta/[^0.3]", transitive_headers=True, transitive_libs=True)
-        self.requires("arba-hash/[^0.2]", transitive_headers=True, transitive_libs=True)
+        if self.version >= Version("0.7.0"):
+            self.requires("arba-meta/[^0.5]", transitive_headers=True, transitive_libs=True)
+            self.requires("arba-hash/[^0.3]", transitive_headers=True, transitive_libs=True)
+        else:
+            self.requires("arba-meta/[^0.3]", transitive_headers=True, transitive_libs=True)
+            self.requires("arba-hash/[^0.2]", transitive_headers=True, transitive_libs=True)
     
     def build_requirements(self):
         self.test_requires("gtest/[^1.14]")
