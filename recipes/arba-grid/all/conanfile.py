04323dbadc1da8f06c76ca6a37f6cab3c6fd44f5
Aymeric Pellé <aympelle@gmail.com>
2025-05-05 18:00:10 +0200

    arba-grid: Adds version 0.6.0.

--- recipes/arba-grid/all/conanfile.py
+++ recipes/arba-grid/all/conanfile.py
@@ -53,7 +53,10 @@ class ArbaGridRecipe(ConanFile):
         check_min_cppstd(self, 20)
 
     def requirements(self):
-        self.requires("arba-math/[^0.6]", transitive_headers=True, transitive_libs=True)
+        if self.version >= Version("0.6.0"):
+            self.requires("arba-math/[^0.7]", transitive_headers=True, transitive_libs=True)
+        else:
+            self.requires("arba-math/[^0.6]", transitive_headers=True, transitive_libs=True)
     
     def build_requirements(self):
         self.test_requires("gtest/[^1.14]")
