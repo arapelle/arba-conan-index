43d68c0a46dfcf5d43f5126fd8967d56df2bf203
Aymeric Pellé <aympelle@gmail.com>
2025-11-16 21:19:09 +0100

    arba-hash: Adds version 0.4.0.

--- recipes/arba-hash/all/test_package/test_package.cpp
+++ recipes/arba-hash/all/test_package/test_package.cpp
@@ -1,11 +1,12 @@
-#include <arba/hash/murmur_hash.hpp>
+#include <arba/hash/murmur_hash_2.hpp>
 #include <arba/hash/version.hpp>
+
 #include <cstdlib>
 #include <iostream>
 
 int main()
 {
-    std::cout << arba::hash::neutral_murmur_hash_64("hello world") << std::endl;
+    std::cout << arba::hash::murmur_hash_2_u64("hello world", cppx::endianness_specific) << std::endl;
     std::cout << "TEST PACKAGE SUCCESS" << std::endl;
     return EXIT_SUCCESS;
 }
